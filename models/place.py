e2abf77b61a85e515a19ebcc79f5dad09f5ce0d5
yadsashel <ysalhi600@gmail.com>
2024-04-17 17:48:06 -0700

    place.py

--- models/place.py
+++ models/place.py
@@ -1,17 +1,8 @@
-#!/usr/bin/python3
 '''doc'''
 from models.base_model import BaseModel
 
-class Place(BaseModel):
-    '''Place Class'''
-    city_id = ""
-    user_id = ""
+
+class City(BaseModel):
+    '''City Class'''
+    state_id = ""
     name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
